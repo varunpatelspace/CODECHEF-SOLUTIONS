2abfc6bb02cd6a8afe52b9ea9ba0cc0baffcc0be
Varun Patel <varunspace541@gmail.com>
2026-07-29 20:08:00 +0530

    Solved VARDIGS [C++​]

---
+++ 29-07-26/VARDIGS.cpp
@@ -0,0 +1,22 @@
+/*
+ ╔═══════════════════════════════════════════════════════════════════════╗
+ ║  Problem  : VARDIGS                                                     ║
+ ║  Platform : CodeChef                                                  ║
+ ║  Status   : Accepted                                                    ║
+ ║  Date     : July 29, 2026                                               ║
+ ║  URL      : https://www.codechef.com/START249D/problems/VARDIGS         ║
+ ╚═══════════════════════════════════════════════════════════════════════╝
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int X;   // your code goes here
+   cin>>X;
+   if(X%11==0){
+       cout<<"NO";
+   }else {
+       cout<<"YES";
+   }
+}
