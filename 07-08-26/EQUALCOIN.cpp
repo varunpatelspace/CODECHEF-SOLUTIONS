92da9237b26a02ca0ce1764e98a36e51a3166b01
Varun Patel <varunspace541@gmail.com>
2026-08-07 19:27:38 +0530

    Solved EQUALCOIN [C++​]

---
+++ 07-08-26/EQUALCOIN.cpp
@@ -0,0 +1,28 @@
+/*
+ ╔═══════════════════════════════════════════════════════════════════════╗
+ ║  Problem  : EQUALCOIN                                                   ║
+ ║  Platform : CodeChef                                                  ║
+ ║  Status   : Accepted                                                    ║
+ ║  Date     : August 7, 2026                                              ║
+ ║  URL      : https://www.codechef.com/problems/EQUALCOIN                 ║
+ ╚═══════════════════════════════════════════════════════════════════════╝
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int t,x,y;   // your code goes here
+   cin>>t;
+   while(t--){
+       cin>>x >>y;
+      int total=x+2*y;
+       if(total%2==0){
+        cout<<"YES\n";  
+       } else{
+           cout<<"NO\n";
+       }
+   }
+   return 0;
+
+}
