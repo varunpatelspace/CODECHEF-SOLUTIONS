895ba9f58c20f240c8c205d363564c923cf2f8a6
Varun Patel <varunspace541@gmail.com>
2026-08-12 20:07:48 +0530

    Solved CHKEV [C++​]

---
+++ 12-08-26/CHKEV.cpp
@@ -0,0 +1,24 @@
+/*
+ ╔═══════════════════════════════════════════════════════════════════════╗
+ ║  Problem  : CHKEV                                                       ║
+ ║  Platform : CodeChef                                                  ║
+ ║  Status   : Accepted                                                    ║
+ ║  Date     : August 12, 2026                                             ║
+ ║  URL      : https://www.codechef.com/START251D/problems/CHKEV           ║
+ ╚═══════════════════════════════════════════════════════════════════════╝
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int l,r;  // your code goes here
+   cin>>l>>r;
+  if(l==r){
+      cout<<"No";
+  }else{
+      cout<<"Yes";
+  }
+  return 0;
+
+}
