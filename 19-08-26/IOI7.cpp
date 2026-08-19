dbf3a43af429c5efb94345f807db345a053ace56
Varun Patel <varunspace541@gmail.com>
2026-08-19 20:03:42 +0530

    Solved IOI7 [C++​]

---
+++ 19-08-26/IOI7.cpp
@@ -0,0 +1,24 @@
+/*
+ ╔═══════════════════════════════════════════════════════════════════════╗
+ ║  Problem  : IOI7                                                        ║
+ ║  Platform : CodeChef                                                  ║
+ ║  Status   : Accepted                                                    ║
+ ║  Date     : August 19, 2026                                             ║
+ ║  URL      : https://www.codechef.com/START252D/problems/IOI7            ║
+ ╚═══════════════════════════════════════════════════════════════════════╝
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int n,g;    // your code goes here
+    cin>>n >>g;
+    if(n>=g){
+        cout<<"Yes";
+        
+    }
+    else{
+        cout<<"No";
+    }
+}
