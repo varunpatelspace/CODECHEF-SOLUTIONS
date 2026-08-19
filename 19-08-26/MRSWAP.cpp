518ad482af880c1bd6e7f0b52c3e25fd949cc42a
Varun Patel <varunspace541@gmail.com>
2026-08-19 20:18:17 +0530

    Solved MRSWAP [C++​]

---
+++ 19-08-26/MRSWAP.cpp
@@ -0,0 +1,34 @@
+/*
+ ╔═══════════════════════════════════════════════════════════════════════╗
+ ║  Problem  : MRSWAP                                                      ║
+ ║  Platform : CodeChef                                                  ║
+ ║  Status   : Accepted                                                    ║
+ ║  Date     : August 19, 2026                                             ║
+ ║  URL      : https://www.codechef.com/START252D/problems/MRSWAP          ║
+ ╚═══════════════════════════════════════════════════════════════════════╝
+ */
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[2 * n + 1];
+        for (int i = 1; i <= 2 * n; i++) {
+            cin >> a[i];
+        }
+
+        int ans = 0;
+
+        for (int i = 1; i <= n; i++) {
+            ans += max(a[i], a[2 * n + 1 - i])
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
